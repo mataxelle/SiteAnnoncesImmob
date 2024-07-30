a5591c89edee59e665d6458427960cb60ae1f20b
axelle <kadance9720@gmail.com>
2024-07-30 17:08:22 +0200

    Edit a property

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -61,24 +61,28 @@ class PropertyController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Property $property)
     {
-        //
+        return view('admin.property.create', [
+            'property' => $property
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PropertyFormRequest $request, Property $property)
     {
-        //
+        $property->update($request->validated());
+        return to_route('admin.property.index')->with('success', 'La propriété a bien été modifié');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Property $property)
     {
-        //
+        $property->delete();
+        return to_route('admin.property.index')->with('success', 'La propriété a bien été supprimé');
     }
 }
--- resources/views/admin/property/index.blade.php
+++ resources/views/admin/property/index.blade.php
@@ -4,35 +4,44 @@
 
 @section('content')
 
-<div class="d-flex justify-content-between align-items-center">
-    <h1 class="mb-5">@yield('title')</h1>
-    <a href="{{ route('admin.property.create') }}" class="btn btn-primary">Ajouter une propriété</a>
-</div>
+    <div class="d-flex justify-content-between align-items-center">
+        <h1 class="mb-5">@yield('title')</h1>
+        <a href="{{ route('admin.property.create') }}" class="btn btn-primary">Ajouter une propriété</a>
+    </div>
 
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Titre </th>
-            <th>Surface </th>
-            <th>Prix </th>
-            <th>Ville </th>
-            <th class="text-end">Actions </th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($properties as $property)
+    <table class="table table-striped">
+        <thead>
             <tr>
-                <td>{{ $property->title }}</td>
-                <td>{{ $property->surface }}m²</td>
-                <td>{{ number_format($property->price, thousands_separator: ' ') }}</td>
-                <td>{{ $property->city }}</td>
-                <td></td>
+                <th>Titre </th>
+                <th>Surface </th>
+                <th>Prix </th>
+                <th>Ville </th>
+                <th class="text-end">Actions </th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($properties as $property)
+                <tr>
+                    <td>{{ $property->title }}</td>
+                    <td>{{ $property->surface }}m²</td>
+                    <td>{{ number_format($property->price, thousands_separator: ' ') }}</td>
+                    <td>{{ $property->city }}</td>
+                    <td>
+                        <div class="d-flex gap-2 w-100 justify-content-end">
+                            <a href="{{ route('admin.property.edit', $property) }}" class="btn btn-primary">Modifier</a>
+                            <form action="{{ route('admin.property.destroy', $property) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger">Supprimer</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-{{ $properties->links() }}
+    {{ $properties->links() }}
 
 
 @endsection
