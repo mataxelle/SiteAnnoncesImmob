c82c036e8facefd340b559e257fab1dba16d80ee
axelle <kadance9720@gmail.com>
2024-08-02 20:46:22 +0200

    Add errors alert

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -57,6 +57,16 @@
             </div>
         @endif
 
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="my-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         @yield('content')
     </div>
 
