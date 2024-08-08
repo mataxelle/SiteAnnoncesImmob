13cf61191e6d56f8ff87bfb89dcf70296d1f2c4e
axelle <kadance9720@gmail.com>
2024-08-08 19:28:44 +0200

    Add properties search request

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchPropertiesRequest;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    public function index()
+    public function index(SearchPropertiesRequest $request)
     {
-        $properties = Property::paginate(16);
+        $query = Property::query();
+
+        if ($request->validated('surface')) {
+            $query = $query->where('surface', '>=', $request->validated('surface'));
+        }
+
+        if ($request->validated('price')) {
+            $query = $query->where('price', '<=', $request->validated('price'));
+        }
+        if ($request->validated('rooms')) {
+            $query = $query->where('rooms', '>=', $request->validated('rooms'));
+        }
+        if ($request->validated('title')) {
+            $query = $query->where('title', 'like', "%{$request->validated('title')}%");
+        }
 
         return view('property.index', [
-            'properties' => $properties
+            'properties' => $query->paginate(16),
+            'input' => $request->validated()
         ]);
     }
 
--- resources/views/property/index.blade.php
+++ resources/views/property/index.blade.php
@@ -6,12 +6,30 @@
 
     <div class="container">
         <h2 class="my-5">Toutes les propriétés</h2>
+
+        <div class="bg-light p-5 mb-5 text-center">
+            <form action="" method="get" class="d-flex gap-2">
+                <input type="number" placeholder="Surface min" class="form-control" name="surface"
+                    value="{{ $input['surface'] ?? '' }}">
+                <input type="number" placeholder="Budget max" class="form-control" name="price"
+                    value="{{ $input['price'] ?? '' }}">
+                <input type="number" placeholder="Nombre de pièces min" class="form-control" name="rooms"
+                    value="{{ $input['rooms'] ?? '' }}">
+                <input placeholder="Mots clé" class="form-control" name="title" value="{{ $input['title'] ?? '' }}">
+                <button class="btn btn-primary btn-sm flex-grow-0">Rechercher</button>
+            </form>
+        </div>
+
         <div class="row">
-            @foreach ($properties as $property)
+            @forelse ($properties as $property)
                 <div class="col-3 mb-3">
                     @include('property.card')
                 </div>
-            @endforeach
+            @empty
+                <div class="col text-center mb-3">
+                    Auncu résultat
+                </div>
+            @endforelse
         </div>
 
         {{ $properties->links() }}
