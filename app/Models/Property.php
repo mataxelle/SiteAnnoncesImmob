2b81c3ec4c5fa65563cd9ba8dff91883ff5e386b
axelle <kadance9720@gmail.com>
2024-09-27 17:50:46 +0200

    Add eloquent softDeleted to properties

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Property extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'title',
