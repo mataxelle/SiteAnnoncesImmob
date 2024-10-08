e07ec66a8f2210a466048dc410ef393631a0f02a
axelle <kadance9720@gmail.com>
2024-10-08 17:33:41 +0200

    Update title and price style

--- resources/views/property/card.blade.php
+++ resources/views/property/card.blade.php
@@ -1,11 +1,12 @@
 <div class="card">
     <div class="card-body">
         <h5 class="card-title">
-            <a href="{{ route('property.show', ['slug' => $property->getSlug(), 'property' => $property]) }}">{{ $property->title }}
+            <a href="{{ route('property.show', ['slug' => $property->getSlug(), 'property' => $property]) }}"
+                class="text-decoration-none title-price-color">{{ $property->title }}
             </a>
         </h5>
         <p class="card-text">{{ $property->surface }}m² - {{ $property->city }} ({{ $property->postal_code }})</p>
-        <div class="text-primary fw-bold">
+        <div class="fw-bold title-price-color">
             {{ number_format($property->price, thousands_separator: ' ') }}€
         </div>
     </div>
